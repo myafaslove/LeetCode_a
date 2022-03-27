03dd9d3c83355afdae159840c4dc9a9711a38a9e
myafaslove <771102443@qq.com>
2022-03-27 23:13:41 +0800

    test

--- DS0/217/main.cpp
+++ DS0/217/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include<iostream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "hello world!!" << endl;
     return 0;
 }

ce98988ef4b01746d26500929c113f1b960555be
myafaslove <771102443@qq.com>
2022-03-31 18:02:12 +0800

    pta1001

--- PTA/1001/main.cpp
+++ PTA/1001/main.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
+int reTimes(int num) {
+    int flag = 0;
+    while (num != 1) {
+//        cout << num << endl;
+        if (num % 2 == 0) {
+            num = num / 2;
+            flag += 1;
+        } else {
+            num = (3 * num + 1) / 2;
+            flag += 1;
+        }
+//        cout << flag << endl;
+    }
+    return flag;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int num;
+    cin >> num;
+    cout << reTimes(num) << endl;
     return 0;
 }

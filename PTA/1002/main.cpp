d04f73b6e6a65b9f0b7550895d79e5c6c0412d44
myafaslove <771102443@qq.com>
2022-04-01 00:20:02 +0800

    1002

--- PTA/1002/main.cpp
+++ PTA/1002/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include<string>
+
+using namespace std;
+
+int sumAll(string n) {
+    int sum = 0;
+    for (int i = 0; i < n.length(); i++) {
+//        cout << n[i] << endl;//测试能否你读出数字
+        sum = sum + (n[i] - 48);
+    }
+    return sum;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string py[10] = {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "bai", "jiu"};
+    cout << py[0] << endl;
+    string num = "1234567890987654321123456789";
+
     return 0;
 }
